8a95dee89921ef77963fd3b6464a9b60fd707457
Unknown <chencheng502@gmail.com>
2017-02-10 22:21:35 -0500

    try blocked

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -1,10 +1,10 @@
-/* 
+/*
     Please include compiler name below (you may also include any other modules you would like to be loaded)
 
 COMPILER= gnu
 
     Please include All compiler flags and libraries as you want them run. You can simply copy this over from the Makefile's first few lines
- 
+
 CC = cc
 OPT = -O3
 CFLAGS = -Wall -std=gnu99 $(OPT)
@@ -27,25 +27,25 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; ++i)
-    /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
+  for (int j = 0; j < N; ++j)
+    /* For each column j of B */
+    for (int k = 0; k < K; ++k)
     {
       /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k)
-	cij += A[i+k*lda] * B[k+j*lda];
-      C[i+j*lda] = cij;
+      //double cij = C[i+j*lda];
+      for (int i = 0; i < M; ++i)
+	       C[i+j*lda] += A[i+k*lda] * B[k+j*lda];
+      //C[i+j*lda] = cij;
     }
 }
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
- * where A, B, and C are lda-by-lda matrices stored in column-major format. 
- * On exit, A and B maintain their input values. */  
+ * where A, B, and C are lda-by-lda matrices stored in column-major format.
+ * On exit, A and B maintain their input values. */
 void square_dgemm (int lda, double* A, double* B, double* C)
 {
-  /* For each block-row of A */ 
+  /* For each block-row of A */
   for (int i = 0; i < lda; i += BLOCK_SIZE)
     /* For each block-column of B */
     for (int j = 0; j < lda; j += BLOCK_SIZE)
