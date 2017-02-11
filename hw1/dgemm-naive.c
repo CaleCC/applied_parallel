101667ef19edb468b5461ab32a6048b525610e95
Unknown <chencheng502@gmail.com>
2017-02-10 21:49:24 -0500

    a

--- hw1/dgemm-naive.c
+++ hw1/dgemm-naive.c
@@ -22,14 +22,12 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 void square_dgemm (int n, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int j = 0; j < n; ++j)
+  for (int i = 0; i < n; ++i)
     /* For each column j of B */
     for (int k = 0; k < n; ++k)
     {
       /* Compute C(i,j) */
-
-      for( int i = 0; i < n; i++ )
-
+      for( int j = 0; j < n; j++ )
 	       C[i+j*n]+= A[i+k*n] * B[k+j*n];
     //  C[i + j * n ] = Cij;
     }
