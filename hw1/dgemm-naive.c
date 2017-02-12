3236bcc3e7b730e1b1ab3cedc11988507f00f741
Unknown <chencheng502@gmail.com>
2017-02-11 23:32:46 -0500

    transpose

--- hw1/dgemm-naive.c
+++ hw1/dgemm-naive.c
@@ -46,7 +46,7 @@ void square_dgemm ( int n, double* A, double* B, double* C )
         for( int i = 0; i < n; ++i )
         {
                 /* For each column j of B */
-                for( int k = 0; j < k; ++k )
+                for( int k = 0; k < n; ++k )
                 {
                         /* Compute C(i,j) */
                         //double cij = C[ i + j*n ];
