42d1a1754d47eeaf6f552d3848c980ca3f816329
Unknown <chencheng502@gmail.com>
2017-02-22 16:50:01 -0500

    dd

--- hw1/dgemm-naive.c
+++ hw1/dgemm-naive.c
@@ -13,7 +13,13 @@ void Mymulti(int n, double *A, double* B,double *C){
   //printf("debig 2\n");
   register double
     c_00_reg, c_01_reg, c_02_reg, c_03_reg,
-    a_0l_reg;//A(0,l)
+    c_10_reg, c_11_reg, c_12_reg, c_13_reg,
+    c_20_reg, c_21_reg, c_22_reg, c_23_reg,
+    c_30_reg, c_31_reg, c_32_reg, c_33_reg,
+    a_0l_reg,//A(0,l)
+    a_1l_reg,
+    a_2l_reg,
+    a_3l_reg;
   double
     *bl0_pntr, *bl1_pntr, *bl2_pntr, *bl3_pntr;
 
@@ -26,45 +32,56 @@ void Mymulti(int n, double *A, double* B,double *C){
   c_01_reg = 0.0;
   c_02_reg = 0.0;
   c_03_reg = 0.0;
-  //printf("debig 3\n");
-  for(l = 0; l < n; l += 4){
+  c_10_reg = 0.0;
+  c_11_reg = 0.0;
+  c_12_reg = 0.0;
+  c_13_reg = 0.0;
+  c_20_reg = 0.0;
+  c_21_reg = 0.0;
+  c_22_reg = 0.0;
+  c_23_reg = 0.0;  //printf("debig 3\n");
+  c_30_reg = 0.0;
+  c_31_reg = 0.0;
+  c_32_reg = 0.0;
+  c_33_reg = 0.0;
+  for(l = 0; l < n; l ++){
     a_0l_reg = A(0,l);
-
+    a_1l_reg = A(1,l);
+    a_2l_reg = A(2,l);
+    a_3l_reg = A(3,l);
+    //first row of C
     c_00_reg += a_0l_reg * *(bl0_pntr);
     c_01_reg += a_0l_reg * *(bl1_pntr);
     c_02_reg += a_0l_reg * *(bl2_pntr);
     c_03_reg += a_0l_reg * *(bl3_pntr);
-    //printf("debig 4\n");
-    a_0l_reg = A(0,l+1);
 
-    c_00_reg += a_0l_reg * *(bl0_pntr+1);
-    c_01_reg += a_0l_reg * *(bl1_pntr+1);
-    c_02_reg += a_0l_reg * *(bl2_pntr+1);
-    c_03_reg += a_0l_reg * *(bl3_pntr+1);
 
-    a_0l_reg = A(0,l+2);
+    c_10_reg += a_1l_reg * *(bl0_pntr);
+    c_11_reg += a_1l_reg * *(bl1_pntr);
+    c_12_reg += a_1l_reg * *(bl2_pntr);
+    c_13_reg += a_1l_reg * *(bl3_pntr);
 
-    c_00_reg += a_0l_reg * *(bl0_pntr+2);
-    c_01_reg += a_0l_reg * *(bl1_pntr+2);
-    c_02_reg += a_0l_reg * *(bl2_pntr+2);
-    c_03_reg += a_0l_reg * *(bl3_pntr+2);
 
-    a_0l_reg = A(0,l+3);
+    c_20_reg += a_2l_reg * *(bl0_pntr);
+    c_21_reg += a_2l_reg * *(bl1_pntr);
+    c_22_reg += a_2l_reg * *(bl2_pntr);
+    c_23_reg += a_2l_reg * *(bl3_pntr);
 
-    c_00_reg += a_0l_reg * *(bl0_pntr+3);
-    c_01_reg += a_0l_reg * *(bl1_pntr+3);
-    c_02_reg += a_0l_reg * *(bl2_pntr+3);
-    c_03_reg += a_0l_reg * *(bl3_pntr+3);
 
-    bl0_pntr+=4;
-    bl1_pntr+=4;
-    bl2_pntr+=4;
-    bl3_pntr+=4;
+    c_30_reg += a_3l_reg * *(bl0_pntr);
+    c_31_reg += a_3l_reg * *(bl1_pntr);
+    c_32_reg += a_3l_reg * *(bl2_pntr);
+    c_33_reg += a_3l_reg * *(bl3_pntr);
+
+    bl0_pntr+=1;
+    bl1_pntr+=1;
+    bl2_pntr+=1;
+    bl3_pntr+=1;
   }
-  C(0,0) += c_00_reg;
-  C(0,1) += c_01_reg;
-  C(0,2) += c_02_reg;
-  C(0,3) += c_03_reg;
+  C(0,0) += c_00_reg; C(0,1) += c_01_reg; C(0,2) += c_02_reg; C(0,3) += c_03_reg;
+  C(1,0) += c_10_reg; C(1,1) += c_11_reg; C(1,2) += c_12_reg; C(1,3) += c_13_reg;
+  C(2,0) += c_20_reg; C(2,1) += c_21_reg; C(2,2) += c_22_reg; C(2,3) += c_23_reg;
+  C(3,0) += c_30_reg; C(3,1) += c_31_reg; C(3,2) += c_32_reg; C(3,3) += c_33_reg;
 }
 
 void square_dgemm ( int n, double* A, double* B, double* C )
@@ -76,7 +93,7 @@ void square_dgemm ( int n, double* A, double* B, double* C )
     //printf("debig 1\n");
   for(j = 0; j < n; j+=4){
     //for each row of C
-    for(i = 0 ; i < n; i+=1){
+    for(i = 0 ; i < n; i+=4){
       //printf("debig 1\n");
       Mymulti(n, &A(i,0),&B(0,j),&C(i,j));
     }
