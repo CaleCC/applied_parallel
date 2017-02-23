30885224d2badc3499119cd1450540a03167ac3c
Unknown <chencheng502@gmail.com>
2017-02-22 21:45:21 -0500

    dd

--- hw1/dgemm-naive.c
+++ hw1/dgemm-naive.c
@@ -101,7 +101,10 @@ void square_dgemm ( int n, double* A, double* B, double* C )
     for(i = 0 ; i < n; i+=4){
       //printf("debig 1\n");
       if(i + 4 > n){
-        AddDot(n, &A(i,0),&B(0,j),&C(i,j));
+        while(i < n){
+          AddDot(n, &A(i,0),&B(0,j),&C(i,j));
+          i++;
+        }
       }else{
         Mymulti(n, &A(i,0),&B(0,j),&C(i,j));
       }
