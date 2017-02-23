fc465a756b56d3fcf92c6a0a234299bf373c25f7
Unknown <chencheng502@gmail.com>
2017-02-23 05:01:39 -0500

    ddd

--- hw1/dgemm-naive.c
+++ hw1/dgemm-naive.c
@@ -12,10 +12,74 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 #include <emmintrin.h>
 #include <immintrin.h>
 
+#include <mmintrin.h>
+#include <xmmintrin.h>  // SSE
+#include <pmmintrin.h>  // SSE2
 
+
+typedef union
+{
+  __m128d v;
+  double d[2];
+} v2df_t;
 void Mymulti(int n, double *A,int lda, double* B,int  ldb,
                                        double *C, int ldc){
-   __m256d c1 = _mm256_loadu_pd(&C(0,0));	// load first row of C
+
+   int p;
+   v2df_t
+     c_00_c_10_vreg,    c_01_c_11_vreg,    c_02_c_12_vreg,    c_03_c_13_vreg,
+     c_20_c_30_vreg,    c_21_c_31_vreg,    c_22_c_32_vreg,    c_23_c_33_vreg,
+     a_0p_a_1p_vreg,
+     a_2p_a_3p_vreg,
+     b_p0_vreg, b_p1_vreg, b_p2_vreg, b_p3_vreg;
+
+   c_00_c_10_vreg.v = _mm_setzero_pd();
+   c_01_c_11_vreg.v = _mm_setzero_pd();
+   c_02_c_12_vreg.v = _mm_setzero_pd();
+   c_03_c_13_vreg.v = _mm_setzero_pd();
+   c_20_c_30_vreg.v = _mm_setzero_pd();
+   c_21_c_31_vreg.v = _mm_setzero_pd();
+   c_22_c_32_vreg.v = _mm_setzero_pd();
+   c_23_c_33_vreg.v = _mm_setzero_pd();
+
+   for ( p=0; p<k; p++ ){
+     a_0p_a_1p_vreg.v = _mm_load_pd( (double *) a );
+     a_2p_a_3p_vreg.v = _mm_load_pd( (double *) ( a+2 ) );
+     a += 4;
+
+     b_p0_vreg.v = _mm_loaddup_pd( (double *) b );       /* load and duplicate */
+     b_p1_vreg.v = _mm_loaddup_pd( (double *) (b+1) );   /* load and duplicate */
+     b_p2_vreg.v = _mm_loaddup_pd( (double *) (b+2) );   /* load and duplicate */
+     b_p3_vreg.v = _mm_loaddup_pd( (double *) (b+3) );   /* load and duplicate */
+
+     b += 4;
+
+     /* First row and second rows */
+     c_00_c_10_vreg.v += a_0p_a_1p_vreg.v * b_p0_vreg.v;
+     c_01_c_11_vreg.v += a_0p_a_1p_vreg.v * b_p1_vreg.v;
+     c_02_c_12_vreg.v += a_0p_a_1p_vreg.v * b_p2_vreg.v;
+     c_03_c_13_vreg.v += a_0p_a_1p_vreg.v * b_p3_vreg.v;
+
+     /* Third and fourth rows */
+     c_20_c_30_vreg.v += a_2p_a_3p_vreg.v * b_p0_vreg.v;
+     c_21_c_31_vreg.v += a_2p_a_3p_vreg.v * b_p1_vreg.v;
+     c_22_c_32_vreg.v += a_2p_a_3p_vreg.v * b_p2_vreg.v;
+     c_23_c_33_vreg.v += a_2p_a_3p_vreg.v * b_p3_vreg.v;
+   }
+
+   C( 0, 0 ) += c_00_c_10_vreg.d[0];  C( 0, 1 ) += c_01_c_11_vreg.d[0];
+   C( 0, 2 ) += c_02_c_12_vreg.d[0];  C( 0, 3 ) += c_03_c_13_vreg.d[0];
+
+   C( 1, 0 ) += c_00_c_10_vreg.d[1];  C( 1, 1 ) += c_01_c_11_vreg.d[1];
+   C( 1, 2 ) += c_02_c_12_vreg.d[1];  C( 1, 3 ) += c_03_c_13_vreg.d[1];
+
+   C( 2, 0 ) += c_20_c_30_vreg.d[0];  C( 2, 1 ) += c_21_c_31_vreg.d[0];
+   C( 2, 2 ) += c_22_c_32_vreg.d[0];  C( 2, 3 ) += c_23_c_33_vreg.d[0];
+
+   C( 3, 0 ) += c_20_c_30_vreg.d[1];  C( 3, 1 ) += c_21_c_31_vreg.d[1];
+   C( 3, 2 ) += c_22_c_32_vreg.d[1];  C( 3, 3 ) += c_23_c_33_vreg.d[1];
+
+  /* __m256d c1 = _mm256_loadu_pd(&C(0,0));	// load first row of C
  	__m256d c2 = _mm256_loadu_pd(&C(1,0));	// load second row of C
  	__m256d c3 = _mm256_loadu_pd(&C(2,0));	// load third row of C
  	__m256d c4 = _mm256_loadu_pd(&C(3,0));	// load fourth row of C
@@ -37,7 +101,7 @@ void Mymulti(int n, double *A,int lda, double* B,int  ldb,
   _mm256_storeu_pd(&C(0,0), c1);
   _mm256_storeu_pd(&C(1,0), c2);
   _mm256_storeu_pd(&C(2,0), c3);
-  _mm256_storeu_pd(&C(3,0), c4);
+  _mm256_storeu_pd(&C(3,0), c4);*/
   // /* So, this routine computes a 4x4 block of matrix A
   //          C( 0, 0 ), C( 0, 1 ), C( 0, 2 ), C( 0, 3 ).
   //          C( 1, 0 ), C( 1, 1 ), C( 1, 2 ), C( 1, 3 ).
