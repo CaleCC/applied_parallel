c3c3a904fa86d5a5c6afa4f7cd308e42486d7a7d
Unknown <chencheng502@gmail.com>
2017-02-22 23:46:34 -0500

    sdf

--- hw1/dgemm-naive.c
+++ hw1/dgemm-naive.c
@@ -66,18 +66,18 @@ void Mymulti(int n, double *A, double* B,double *C){
   for ( p=0; p<n; p++ ){
     a_0p_a_1p_vreg.v = _mm_load_pd( (double *) &A( 0, p ) );
     a_2p_a_3p_vreg.v = _mm_load_pd( (double *) &A( 2, p ) );
-
+    printf("ddd9\n");
     b_p0_vreg.v = _mm_loaddup_pd( (double *) b_p0_pntr++ );   /* load and duplicate */
     b_p1_vreg.v = _mm_loaddup_pd( (double *) b_p1_pntr++ );   /* load and duplicate */
     b_p2_vreg.v = _mm_loaddup_pd( (double *) b_p2_pntr++ );   /* load and duplicate */
     b_p3_vreg.v = _mm_loaddup_pd( (double *) b_p3_pntr++ );   /* load and duplicate */
-
+printf("ddd10\n");
     /* First row and second rows */
     c_00_c_10_vreg.v += a_0p_a_1p_vreg.v * b_p0_vreg.v;
     c_01_c_11_vreg.v += a_0p_a_1p_vreg.v * b_p1_vreg.v;
     c_02_c_12_vreg.v += a_0p_a_1p_vreg.v * b_p2_vreg.v;
     c_03_c_13_vreg.v += a_0p_a_1p_vreg.v * b_p3_vreg.v;
-
+printf("ddd11\n");
     /* Third and fourth rows */
     c_20_c_30_vreg.v += a_2p_a_3p_vreg.v * b_p0_vreg.v;
     c_21_c_31_vreg.v += a_2p_a_3p_vreg.v * b_p1_vreg.v;
