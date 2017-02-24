0881bee5d9670f50431790f97e3d830c054d414a
Unknown <chencheng502@gmail.com>
2017-02-23 23:26:28 -0500

    clean version

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -24,7 +24,7 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 #define A(i,j) A[(j)*lda + (i)]
 #define B(i,j) B[(j)*ldb + (i)]
 #define C(i,j) C[(j)*ldc + (i)]
-//void Mymulti(int, double*,  double*, double *)
+
 
 #include <emmintrin.h>
 #include <immintrin.h>
@@ -35,9 +35,9 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 
 typedef union
 {
-  //__m128d vector;
+
   __m256d vector2;
-  //double data[2];
+
   double data2[4];
 } v2df_t;
 
@@ -46,14 +46,7 @@ void Mymulti(int k, double *A,int lda, double* B,int  ldb,
 
    int p;
    v2df_t
-    //  c_00_c_10_vreg,    c_01_c_11_vreg,    c_02_c_12_vreg,    c_03_c_13_vreg,
-    //  c_20_c_30_vreg,    c_21_c_31_vreg,    c_22_c_32_vreg,    c_23_c_33_vreg,
 
-    //  a_0p_a_1p_vreg,
-    //  a_2p_a_3p_vreg,
-     //
-    //
-    //  b_p0_vreg, b_p1_vreg, b_p2_vreg, b_p3_vreg;
 
      c_00_10_20_30_vreg,
      c_02_12_22_32_vreg,
@@ -68,14 +61,6 @@ void Mymulti(int k, double *A,int lda, double* B,int  ldb,
      b_p3_vreg;
 
 
-  //  c_00_c_10_vreg.vector = _mm_setzero_pd();
-  //  c_01_c_11_vreg.vector = _mm_setzero_pd();
-  //  c_02_c_12_vreg.vector = _mm_setzero_pd();
-  //  c_03_c_13_vreg.vector = _mm_setzero_pd();
-  //  c_20_c_30_vreg.vector = _mm_setzero_pd();
-  //  c_21_c_31_vreg.vector = _mm_setzero_pd();
-  //  c_22_c_32_vreg.vector = _mm_setzero_pd();
-  //  c_23_c_33_vreg.vector = _mm_setzero_pd();
 
 
    c_00_10_20_30_vreg.vector2 = _mm256_setzero_pd();
@@ -85,71 +70,36 @@ void Mymulti(int k, double *A,int lda, double* B,int  ldb,
 
 
 
-   //c_00_c_10_vreg.vector = _mm256_setzero_pd();
-   //c_01_c_11_vreg.vector = _mm256_setzero_pd();
-   //c_02_c_12_vreg.vector = _mm256_setzero_pd();
-   //c_03_c_13_vreg.vector = _mm256_setzero_pd();
-   //c_20_c_30_vreg.vector = _mm256_setzero_pd();
-   //c_21_c_31_vreg.vector = _mm256_setzero_pd();
-   //c_22_c_32_vreg.vector = _mm256_setzero_pd();
-   //c_23_c_33_vreg.vector = _mm256_setzero_pd();
+
 
    for ( p=0; p<k; p++ ){
      a_0p_1p_2p_3p_vreg.vector2 =  _mm256_load_pd( (double *) A );
-     //a_0p_a_1p_vreg.vector = _mm256_load_pd( (double *) A );
-     //a_2p_a_3p_vreg.vector = _mm256_load_pd( (double *) ( A+2 ) );
-    //  a_0p_a_1p_vreg.vector = _mm_load_pd( (double *) A );
-    //  a_2p_a_3p_vreg.vector = _mm_load_pd( (double *) ( A+2 ) );
+
      A += 4;
 
-    //  b_p0_vreg.vector = _mm_loaddup_pd( (double *) B );	   /* load and duplicate */
-    //  b_p1_vreg.vector = _mm_loaddup_pd( (double *) (B+1) );   /* load and duplicate */
-    //  b_p2_vreg.vector = _mm_loaddup_pd( (double *) (B+2) );   /* load and duplicate */
-    //  b_p3_vreg.vector = _mm_loaddup_pd( (double *) (B+3) );   /* load and duplicate */
 
      b_p0_vreg.vector2 = _mm256_broadcast_sd( (double *) B );	   /* load and duplicate */
      b_p1_vreg.vector2 = _mm256_broadcast_sd( (double *) (B+1) );   /* load and duplicate */
      b_p2_vreg.vector2 = _mm256_broadcast_sd( (double *) (B+2) );   /* load and duplicate */
      b_p3_vreg.vector2 = _mm256_broadcast_sd( (double *) (B+3) );   /* load and duplicate */
-    // b_p0_p1_vreg.vector2 = _mm256_broadcast_sd((double *) B);
-    // b_p2_p3_vreg.vector2 = _mm256_broadcast_sd((double *) (B+2));
+
 
      B += 4;
-     /*first row and second rows*/
+
      c_00_10_20_30_vreg.vector2 += a_0p_1p_2p_3p_vreg.vector2 * b_p0_vreg.vector2;
      c_01_11_21_31_vreg.vector2 += a_0p_1p_2p_3p_vreg.vector2 * b_p1_vreg.vector2;
 
-     /*third and forth rows*/
+
      c_02_12_22_32_vreg.vector2 += a_0p_1p_2p_3p_vreg.vector2 * b_p2_vreg.vector2;
      c_03_13_23_33_vreg.vector2 += a_0p_1p_2p_3p_vreg.vector2 * b_p3_vreg.vector2;
 
 
 
 
-    //  /* First row and second rows */
-    //  c_00_c_10_vreg.vector += a_0p_a_1p_vreg.vector * b_p0_vreg.vector;
-    //  c_01_c_11_vreg.vector += a_0p_a_1p_vreg.vector * b_p1_vreg.vector;
-    //  c_02_c_12_vreg.vector += a_0p_a_1p_vreg.vector * b_p2_vreg.vector;
-    //  c_03_c_13_vreg.vector += a_0p_a_1p_vreg.vector * b_p3_vreg.vector;
-     //
-    //  /* Third and fourth rows */
-    //  c_20_c_30_vreg.vector += a_2p_a_3p_vreg.vector * b_p0_vreg.vector;
-    //  c_21_c_31_vreg.vector += a_2p_a_3p_vreg.vector * b_p1_vreg.vector;
-    //  c_22_c_32_vreg.vector += a_2p_a_3p_vreg.vector * b_p2_vreg.vector;
-    //  c_23_c_33_vreg.vector += a_2p_a_3p_vreg.vector * b_p3_vreg.vector;
+
    }
 
-  //  C( 0, 0 ) += c_00_c_10_vreg.data[0];  C( 0, 1 ) += c_01_c_11_vreg.data[0];
-  //  C( 0, 2 ) += c_02_c_12_vreg.data[0];  C( 0, 3 ) += c_03_c_13_vreg.data[0];
-   //
-  //  C( 1, 0 ) += c_00_c_10_vreg.data[1];  C( 1, 1 ) += c_01_c_11_vreg.data[1];
-  //  C( 1, 2 ) += c_02_c_12_vreg.data[1];  C( 1, 3 ) += c_03_c_13_vreg.data[1];
-   //
-  //  C( 2, 0 ) += c_20_c_30_vreg.data[0];  C( 2, 1 ) += c_21_c_31_vreg.data[0];
-  //  C( 2, 2 ) += c_22_c_32_vreg.data[0];  C( 2, 3 ) += c_23_c_33_vreg.data[0];
-   //
-  //  C( 3, 0 ) += c_20_c_30_vreg.data[1];  C( 3, 1 ) += c_21_c_31_vreg.data[1];
-  //  C( 3, 2 ) += c_22_c_32_vreg.data[1];  C( 3, 3 ) += c_23_c_33_vreg.data[1];
+
 
 
    C( 0, 0 ) += c_00_10_20_30_vreg.data2[0]; C( 0, 1) += c_01_11_21_31_vreg.data2[0];
