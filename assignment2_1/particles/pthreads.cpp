bdf71f46c60383945be3829eb7bcb8500e9d5897
Allen Tung <Snowrries@users.noreply.github.com>
2017-03-17 17:18:47 -0400

    Initial binned pthreads implementation

--- assignment2_1/particles/pthreads.cpp
+++ assignment2_1/particles/pthreads.cpp
@@ -2,14 +2,27 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
-#include <pthread.h>
 #include "common.h"
+#include <vector>
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+//
+//  tuned constants copied from common.cpp
+//
+#define density 0.0005
+#define mass    0.01
+#define cutoff  0.01
+#define min_r   (cutoff/100)
+#define dt      0.0005
 
 //
 //  global variables
 //
 int n, n_threads,no_output=0;
+int num_bins, num_bin_row;
 particle_t *particles;
+vector<vector<particle_t*> > bins;
 FILE *fsave,*fsum;
 pthread_barrier_t barrier;
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
@@ -21,17 +34,42 @@ double gabsmin=1.0,gabsavg=0.0;
 #define P( condition ) {if( (condition) != 0 ) { printf( "\n FAILURE in %s, line %d\n", __FILE__, __LINE__ );exit( 1 );}}
 
 //
+//create bins with length of cutoff
+//
+void create_bins(vector<vector<particle_t*> > &bins, particle_t* particles, int n, int & num_bin_row) {
+	//size = sqrt(density * n)
+	double binsize = 2 * cutoff;
+	//the number of bins in a row = size / binlength. Round up.
+	double size = sqrt(density * n); //This is from common.cpp
+	num_bin_row = ceil(size / binsize);
+	//printf("num_bin_row is %d\n", num_bin_row);
+	//resize the vector to the exact size of bins.
+	//printf("bins.resize\n");
+	bins.resize(num_bin_row * num_bin_row);
+	//put particles in bins according to their locations
+	for (int j = 0; j < n; j++) {
+		int x = floor(particles[j].x / binsize);
+		int y = floor(particles[j].y / binsize);
+		//printf("Pushing back x: %d, y: %d, into %d\n", x, y, x + y*num_bin_row);
+		bins[x + y * num_bin_row].push_back(&particles[j]);
+	}
+	//printf("create_bins completed\n");
+}
+
+//
 //  This is where the action happens
+//  We want each thread to handle a block of local particles, as well as its halo region.
 //
 void *thread_routine( void *pthread_id )
 {
     int navg,nabsavg=0;
     double dmin,absmin=1.0,davg,absavg=0.0;
     int thread_id = *(int*)pthread_id;
+	vector<particle_t*> temp_move;
 
-    int particles_per_thread = (n + n_threads - 1) / n_threads;
-    int first = min(  thread_id    * particles_per_thread, n );
-    int last  = min( (thread_id+1) * particles_per_thread, n );
+    int bins_per_thread = (num_bins + n_threads - 1) / n_threads;
+    int first = min(  thread_id    * bins_per_thread, num_bins);
+    int last  = min( (thread_id+1) * bins_per_thread, num_bins);
     
     //
     //  simulate a number of time steps
@@ -44,12 +82,62 @@ void *thread_routine( void *pthread_id )
         //
         //  compute forces
         //
-        for( int i = first; i < last; i++ )
-        {
-            particles[i].ax = particles[i].ay = 0;
-            for (int j = 0; j < n; j++ )
-                apply_force( particles[i], particles[j], &dmin, &davg, &navg );
-        }
+		for (int i = first; i < last; i++)
+		{
+			vector<particle_t*> binQ = bins[i];
+			int particles_per_bin = binQ.size();
+			//printf("binQ size: %d\t", particles_per_bin);
+			for (int j = 0; j < particles_per_bin; j++) {
+				binQ[j]->ax = binQ[j]->ay = 0;
+			}
+			//printf("Accelerations zeroed\n");
+
+			//Search within current bin and a 'halo region'
+			//Halo region may be defined as the region around the bin of size 'cutoff'
+			//For ease in computation, the neighboring bins may be used as the halo.
+
+			//int cx = floor(particles[i].x/cutoff);
+			//int cy = floor(particles[i].y/cutoff);
+			//int location = cx + num_bin_row * cy;
+			int location = i;
+			vector<int> x_range;
+			vector<int> y_range;
+			// int startx = -1; // the begin row of bin
+			// int endx = 1;
+			// int starty = -1;
+			// int endy = 1;
+			x_range.push_back(0);
+			y_range.push_back(0);
+			if (location >= num_bin_row) {
+				y_range.push_back(-1);
+			}
+			if (location < num_bin_row*(num_bin_row - 1)) {
+				y_range.push_back(1);
+			}
+			if (location % num_bin_row != 0) {
+				x_range.push_back(-1);
+			}
+			if (location % num_bin_row != num_bin_row - 1) {
+				x_range.push_back(1);
+			}
+			//printf("x and y ranges initialized.\n");
+			//This should manage the ranges such that the halo region is searched.
+			for (int a = 0; a < x_range.size(); a++) {
+				for (int b = 0; b < y_range.size(); b++) {
+					int bin_num = i + x_range[a] + num_bin_row*y_range[b];
+					//printf("i: %d, bin_num: %d, bins[i].size(): %d, bins[bin_num].size(): %d\n",i, bin_num, bins[i].size(), bins[bin_num].size());
+
+					for (int c = 0; c < bins[i].size(); c++) {
+						for (int d = 0; d < bins[bin_num].size(); d++) {
+							//printf("apply_force begin");
+							apply_force(*bins[i][c], *bins[bin_num][d], &dmin, &davg, &navg);
+							//printf("apply_force end\n");
+						}
+					}
+				}
+			}
+			//printf("Single bin completed\n");
+		}
         
         pthread_barrier_wait( &barrier );
         
@@ -68,8 +156,40 @@ void *thread_routine( void *pthread_id )
         //
         //  move particles
         //
-        for( int i = first; i < last; i++ ) 
-            move( particles[i] );
+		double binsize = cutoff * 2;
+
+		for (int b = first; b < last; b++)
+		{//Insert logic here
+			int size = bins[b].size();
+			for (int p = 0; p < size;) {
+				//printf("Moving particle in bin %d, p = %d\n", b, p);
+				move(*bins[b][p]);
+
+				int x = floor(bins[b][p]->x / binsize);
+				int y = floor(bins[b][p]->y / binsize);
+				if (y * num_bin_row + x != b)
+				{
+					//printf("p is %d\n", p);
+					//printf("Moving particles from bin %d to %d\n", b, x+y*num_bin_row);
+					temp_move.push_back(bins[b][p]);
+					bins[b].erase(bins[b].begin() + p);
+					size--;
+				}
+				else {
+					p++;
+				}
+			}
+		}
+		for (int i = 0; i < temp_move.size(); i++) {
+			int x = floor(temp_move[i]->x / binsize);
+			int y = floor(temp_move[i]->y / binsize);
+			//printf("Pushing particle into bin[%d]\n", x+y*num_bin_row);
+
+			bins[x + y * num_bin_row].push_back(temp_move[i]);
+			//printf("Pushback complete\n");
+		}
+		temp_move.clear();
+		//printf("temp_move cleared\n");
         
         pthread_barrier_wait( &barrier );
         
@@ -132,6 +252,11 @@ int main( int argc, char **argv )
     particles = (particle_t*) malloc( n * sizeof(particle_t) );
     set_size( n );
     init_particles( n, particles );
+
+	//create the bins to contain the particles
+	num_bin_row = 0;
+	create_bins(bins, particles, n, num_bin_row);
+	num_bins = num_bin_row * num_bin_row;
 
     pthread_attr_t attr;
     P( pthread_attr_init( &attr ) );
