bb2dea172ffe2cb0494e93108e46fde5c937e034
Allen Tung <Snowrries@users.noreply.github.com>
2017-03-21 20:12:33 -0400

    gather the sizes first
    Don't just use partition_sizes, that doesn't even make any sense

--- assignment2_1/particles/mpi.cpp
+++ assignment2_1/particles/mpi.cpp
@@ -337,9 +337,10 @@ int main( int argc, char **argv )
         
         //Handle the zippy particles first.
         int howManyZips[n_proc];
-        MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Allgatherv(localzip.data(), zip, PARTICLE, zippy, partition_sizes, partition_offsets, PARTICLE, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);        
         MPI_Allgather(&zip, 1, MPI_INT, howManyZips, 1, MPI_INT, MPI_COMM_WORLD);
+        MPI_Allgatherv(localzip.data(), zip, PARTICLE, zippy, howManyZips, partition_offsets, PARTICLE, MPI_COMM_WORLD);
+
         for(int i = 0; i < n_proc; i++){
             for(int j = 0; j < howManyZips[i]; j++){
                 int x = floor(zippy[partition_offsets[i]+j].x / binsize);
